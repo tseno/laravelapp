5f9d9f15b76e86f5cd1344055c126d1c7cfd19e1
tseno <tseno26@gmail.com>
2019-03-12 22:15:36 +0900

    add app.css

--- resources/views/layouts/laravelapp.blade.php
+++ resources/views/layouts/laravelapp.blade.php
@@ -2,14 +2,16 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Person/Index</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>@yield('title')</title>
 </head>
 <body>
 <div class="container">
     <h1>@yield('title')</h1>
     @yield('content')
 </div>
+
+<script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
